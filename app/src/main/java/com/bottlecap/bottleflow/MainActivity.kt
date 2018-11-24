d21b7b2be5789a6adbd9a6064d3d47191865f3c5
송채윤 <indexor18@gmail.com>
2018-11-24 22:09:30 +0900

    Remove all test files

--- app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
+++ app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.bottlecap.bottleflow
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // 최상희2
-        yujeong("dbwjd")
-
     }
-    fun yujeong(a: String) {
-        System.out.println(a)
-    }
-
-    //Jujin First Commit
 }
