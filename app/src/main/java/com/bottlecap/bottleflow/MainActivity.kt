a312b7285b487d471346295da907a6e2a57a3934
dbwjd <rladbwjd341@naver.com>
2018-11-25 07:23:32 +0900

    Add Animation fot main activity
    - 메인액티비티에 애니메이션 적용

--- app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
+++ app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
@@ -2,11 +2,29 @@ package com.bottlecap.bottleflow
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setAnimation()
+    }
+
+    fun setAnimation(){
+        var bottle1 : Animation = AnimationUtils.loadAnimation (this, R.anim.bottle1)
+        var bottle2 : Animation = AnimationUtils.loadAnimation (this, R.anim.bottle2)
+        var bottle3 : Animation = AnimationUtils.loadAnimation (this, R.anim.bottle3)
+        var sun : Animation = AnimationUtils.loadAnimation (this, R.anim.sun)
+        mainBottle1.startAnimation(bottle1)
+        mainBottle2.startAnimation(bottle1)
+        mainBottle3.startAnimation(bottle2)
+        mainBottle4.startAnimation(bottle2)
+        mainBottle5.startAnimation(bottle3)
+        mainBottle6.startAnimation(bottle3)
+        mainSunCloud.startAnimation(sun)
     }
 }
