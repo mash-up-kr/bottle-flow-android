7667b91df263cd863a7d1e7e2599023c75a9e5ac
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2018-11-25 04:52:19 +0900

    add activity_shake2.xml

---
+++ app/src/main/java/com/bottlecap/bottleflow/Shake2.kt
@@ -0,0 +1,14 @@
+package com.bottlecap.bottleflow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Shake2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shake2)
+
+
+    }
+}
