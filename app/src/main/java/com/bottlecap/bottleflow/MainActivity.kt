c51f5946787593caf1b52f94e3dd3c8d2bd7b54a
송채윤 <indexor18@gmail.com>
2018-11-25 08:49:50 +0900

    Refactor yujeong's codes

--- app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
+++ app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
@@ -2,8 +2,8 @@ package com.bottlecap.bottleflow
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +14,19 @@ class MainActivity : AppCompatActivity() {
         setAnimation()
     }
 
-    fun setAnimation(){
-        var bottle1: Animation = AnimationUtils.loadAnimation(this, R.anim.bottle1)
-        var bottle2: Animation = AnimationUtils.loadAnimation(this, R.anim.bottle2)
-        var bottle3: Animation = AnimationUtils.loadAnimation(this, R.anim.bottle3)
-        var sun: Animation = AnimationUtils.loadAnimation(this, R.anim.sun)
-        var cloud1: Animation = AnimationUtils.loadAnimation(this, R.anim.cloud1)
-        var cloud2: Animation = AnimationUtils.loadAnimation(this, R.anim.cloud2)
-        mainBottle1.startAnimation(bottle1)
-        mainBottle2.startAnimation(bottle1)
-        mainBottle3.startAnimation(bottle2)
-        mainBottle4.startAnimation(bottle2)
-        mainBottle5.startAnimation(bottle3)
-        mainBottle6.startAnimation(bottle3)
-        mainSun.startAnimation(sun)
-        mainCloud1.startAnimation(cloud1)
-        mainCloud2.startAnimation(cloud2)
+    private fun setAnimation(){
+        startAnimation(mainBottle1, R.anim.bottle1)
+        startAnimation(mainBottle2, R.anim.bottle2)
+        startAnimation(mainBottle3, R.anim.bottle2)
+        startAnimation(mainBottle4, R.anim.bottle2)
+        startAnimation(mainBottle5, R.anim.bottle2)
+        startAnimation(mainBottle6, R.anim.bottle2)
+        startAnimation(mainSun, R.anim.sun)
+        startAnimation(mainCloud1, R.anim.cloud1)
+        startAnimation(mainCloud2, R.anim.cloud2)
+    }
+
+    private fun startAnimation(imageView: ImageView, animationId: Int) {
+        imageView.startAnimation(AnimationUtils.loadAnimation(this, animationId))
     }
 }
