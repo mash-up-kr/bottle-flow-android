2ae5c78d46baac85dfd6d6dade60b116b057b5ee
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2018-11-25 05:24:45 +0900

    apply code reviews

--- app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
+++ app/src/main/java/com/bottlecap/bottleflow/MainActivity.kt
@@ -9,8 +9,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun chaeWon(){
-
-    }
 }
--- app/src/main/java/com/bottlecap/bottleflow/Shake2.kt
+++ app/src/main/java/com/bottlecap/bottleflow/Shake2.kt
@@ -2,6 +2,9 @@ package com.bottlecap.bottleflow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_shake2.*
 
 class Shake2 : AppCompatActivity() {
 
@@ -9,6 +12,13 @@ class Shake2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shake2)
 
+        
+/*
+        btSendShake2.setOnClickListener{
+            if(etShake2.text !== ){
+                Toast.makeText(this, "이모디콘만 입력하세요", Toast.LENGTH_LONG).show();
+            }
+        }*/
 
     }
 }
